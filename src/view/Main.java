711aef2a77720cfe2e51d907ef14e7c6cd0588f2
NymaXx <53951684+NymaXx@users.noreply.github.com>
2020-12-05 12:26:28 -0500

    Anadido de librerias y estructuracion clase Main

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,37 @@
 package view;
 
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import processing.core.PApplet;
 
+	public class Main extends PApplet{
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			PApplet.main(Main.class.getName());
+			
+			
+		}
+		
+		
+		public void settings() {
+			size(700,500);
+			
+		}
+		
+		
+		public void setup() {
+			
+		
+			
+		}
+		
+		
+		public void draw() {
+			background(255);
+	
+		}
+		
+		public void keyPressed() {
+			
+		}
 	}
-
-}
