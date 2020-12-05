219234ebf2cb6b852275154a3a0490f4ce127507
NymaXx <53951684+NymaXx@users.noreply.github.com>
2020-12-05 16:18:51 -0500

    anadido de las imagenes completamente

--- src/model/Character.java
+++ src/model/Character.java
@@ -1,5 +1,27 @@
 package model;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Character {
 
-}
+	private int posX;
+	private int posY;
+	private int speed;
+	private int dir;
+	private PImage anie;
+	private PApplet app;
+	
+	Character(int posX,int posY,int speed, int dir,PApplet app){
+		app= this.app;
+		posX=this.posX;
+		posY=this.posY;
+		speed=this.speed;
+		dir=this.dir;
+		anie= app.loadImage("../images/anie_1.png");
+		
+	}
+
+
+} 
+
